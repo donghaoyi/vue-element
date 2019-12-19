bb141697ef6ca81c001a8fcec4df0df5cac8f38c
donghaoyi <1203350867@qq.com>
2019-12-19 17:44:26 +0800

    first commit

---
+++ src/components/support-ico/support-ico.vue
@@ -0,0 +1,34 @@
+<template>
+  <span class="supprt-icon" :class="iconCls"></span>
+</template>
+<script>
+export default {
+  name: "support-icon",
+  props: {
+    size: {
+      type: Number
+    },
+    type: {
+      type: Number
+    }
+  },
+  computed: {
+    iconCls() {
+      const classMap = [
+        "decrease",
+        "discount",
+        "special",
+        "invoice",
+        "guarantee"
+      ];
+      return `icon-${this.size} ${classMap[this.type]}`;
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+@import '~common/stylus/mixin'
+.support-ico
+  display inline-block
+  background-repeat no-repeat
+</style>
